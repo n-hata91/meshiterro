13ad6990ded36e2dbe1bcda37b24cf93e9dde49a
n-hata91 <htnoms5a@gmail.com>
2020-04-27 09:33:49 +0900

    [Update]5-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7eb4793d942c9ea357d5a68447a576e9674633dce5544cfae9a2b0a43c26c5acdb4e11fba8a0c08b17b7275b069ee5efab3e5fefcf65094a64ec1415ac3d0185'
