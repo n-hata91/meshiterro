46cd187dfb46220e9bca23b3c2bad253f6e9dd14
n-hata91 <htnoms5a@gmail.com>
2020-04-28 18:08:52 +0900

    [update]5-8

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'post_images#index'
   devise_for :users
-  resources :post_images, only: [:new, :create, :show, :index]
+  resources :post_images, only: [:new, :create, :show, :index, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resource :post_comments, only: [:create, :destroy]
+  end
+  resources :users,  only: [:show, :edit, :update,]
 end
